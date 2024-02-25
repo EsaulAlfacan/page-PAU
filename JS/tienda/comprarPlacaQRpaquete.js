91aa6dbda94d84d3a095900d52527b16095c6d3a
EsaulAlfacan <esaulalfacan@gmail.com>
2024-02-24 23:05:04 -0600

    ACTUALIZACION

---
+++ JS/tienda/comprarPlacaQRpaquete.js
@@ -0,0 +1,80 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("enviarWhatsapp")
+    .addEventListener("click", function (event) {
+      event.preventDefault(); // Previene el comportamiento por defecto para asegurar la validación
+
+      // Obtener los datos seleccionados e ingresados por el usuario
+      const zona = document.getElementById("entrega")
+        ? document.getElementById("entrega").value
+        : "";
+      const nombreDueno = document.getElementById("nombreDueno").value.trim();
+      const nombreMascota = document
+        .getElementById("nombreMascota")
+        .value.trim();
+      const direccionEnvio = document
+        .getElementById("direccionEnvio")
+        .value.trim();
+      const codigoPostal = document.getElementById("codigoPostal").value.trim();
+      const telefono1 = document.getElementById("telefono1").value.trim();
+
+      if (
+        !nombreDueno ||
+        !nombreMascota ||
+        !telefono1 ||
+        !direccionEnvio ||
+        !codigoPostal
+      ) {
+        alert("Por favor, completa todos los campos requeridos.");
+        return; // Detiene la ejecución si algún campo requerido está vacío
+      }
+
+      const formaSeleccionada = document.getElementById("opcionesForma").value;
+      const coloresSeleccionados = obtenerColoresSeleccionados();
+      const aditamentosSeleccionados = obtenerAditamentosSeleccionados();
+
+      // Construir el mensaje de WhatsApp
+      let mensaje = `PLACA DE RESINA + QR (INDIVIDUAL) $149 mxm \n
+Zona: ${encodeURIComponent(zona)}
+Nombre del dueño de la mascota: ${encodeURIComponent(nombreDueno)}
+Nombre de la mascota: ${encodeURIComponent(nombreMascota)}
+Dirección de envío: ${encodeURIComponent(direccionEnvio)}
+Código postal: ${encodeURIComponent(codigoPostal)}
+Número de Teléfono: ${encodeURIComponent(telefono1)}
+Forma seleccionada: ${encodeURIComponent(formaSeleccionada)}
+Colores seleccionados:${coloresSeleccionados
+        .map((color) => `\n- ${encodeURIComponent(color)}`)
+        .join("")}
+Aditivos seleccionados:${aditamentosSeleccionados
+        .map((aditamento) => `\n- ${encodeURIComponent(aditamento)}`)
+        .join("")}`;
+
+      // Crear la URL para enviar el mensaje por WhatsApp
+      const url = `https://api.whatsapp.com/send?phone=7751171879&text=${encodeURIComponent(
+        mensaje
+      )}`;
+
+      // Abrir la URL en una nueva pestaña
+      window.open(url);
+    });
+});
+
+function obtenerColoresSeleccionados() {
+  const checkboxes = document.querySelectorAll(
+    '.personalizar-forma-titulo.colores input[name="colores"]:checked'
+  );
+  const coloresSeleccionados = Array.from(checkboxes).map(
+    (checkbox) => checkbox.value
+  );
+  return coloresSeleccionados;
+}
+
+function obtenerAditamentosSeleccionados() {
+  const checkboxes = document.querySelectorAll(
+    '.opciones-colores input[name="aditamento"]:checked'
+  );
+  const aditamentosSeleccionados = Array.from(checkboxes).map(
+    (checkbox) => checkbox.value
+  );
+  return aditamentosSeleccionados;
+}
